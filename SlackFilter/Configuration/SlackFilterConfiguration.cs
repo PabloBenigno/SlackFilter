6c9f108656f7bda46837f4b9f53f3bb427fa244d
Pablo Pérez Camarena <pablo.perez@soderbergpartners.se>
2018-05-12 17:38:44 +0200

    Changed pull request filter to use repository prefix. Removed users list.

--- SlackFilter/Configuration/SlackFilterConfiguration.cs
+++ SlackFilter/Configuration/SlackFilterConfiguration.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.Extensions.Configuration.UserSecrets;
-
-namespace SlackFilter.Configuration
+﻿namespace SlackFilter.Configuration
 {
     public class SlackFilterConfiguration
     {
         public string PersonalToken { get; set; }
         public string VstsBaseAddress { get; set; }
-        public string VsrmBaseAddress { get; set; }
         public TeamConfiguration[] TeamConfigurations { get; set; }
     }
 }
